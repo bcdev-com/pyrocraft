27a36d0bda4ff6363530118a608f2157edf4d261
Blake Coverett <blake@bcdev.com>
2024-01-02 21:36:15 -0800

    initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+import PhotoSwipeLightbox from './photoswipe/photoswipe-lightbox.esm.min.js';
+import PhotoSwipe from './photoswipe/photoswipe.esm.min.js';
+const lightbox = new PhotoSwipeLightbox({
+  gallery: '#gallery',
+  children: 'a',
+  bgOpacity: 0.9,
+  loop: true,
+  wheelToZoom: true,
+  spacing: 0.5,
+  clickToCloseNonZoomable: false,
+  preload: [2, 3],
+  pswpModule: PhotoSwipe
+});
+lightbox.init();
